706b154559e61b9e4592ba77263ad029dd56d671
lipengzhou <lpzmail@163.com>
2019-07-08 12:28:44 +0800

    导入vant 组件库

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {
+  Button,
+  Tabbar,
+  TabbarItem
+} from 'vant'
+
+Vue
+  .use(Button)
+  .use(Tabbar)
+  .use(TabbarItem)
 
 Vue.config.productionTip = false
 
