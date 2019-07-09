053bf4c34ba4c234a17c68c838905ad176798f9f
lipengzhou <lpzmail@163.com>
2019-07-09 12:07:42 +0800

    登录-表单验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -12,6 +12,7 @@
           clearable
           label="手机号"
           placeholder="请输入手机号"
+          :error-message="errors.mobile"
         />
 
         <van-field
@@ -19,6 +20,7 @@
           type="password"
           label="密码"
           placeholder="请输入密码"
+          :error-message="errors.code"
           required
         />
       </van-cell-group>
@@ -47,14 +49,38 @@ export default {
         mobile: '18801185985',
         code: '123456'
       },
-      loginLoading: false // 控制登录按钮的 loading 状态
+      loginLoading: false, // 控制登录按钮的 loading 状态
+      errors: {
+        mobile: '',
+        code: ''
+      }
     }
   },
 
   methods: {
     async handleLogin () {
-      this.loginLoading = true
       try {
+        // 发送请求之前，校验表单数据，校验通过，才进行登录
+        const { mobile, code } = this.user
+        const errors = this.errors
+
+        if (mobile.length) {
+          errors.mobile = ''
+        } else {
+          errors.mobile = '手机号不能为空'
+          return
+        }
+
+        if (code.length) {
+          errors.code = ''
+        } else {
+          errors.code = '验证码不能为空'
+          return
+        }
+
+        // 表单验证通过，发送请求，loading 加载
+        this.loginLoading = true
+
         const data = await login(this.user)
 
         this.$store.commit('setUser', data)
