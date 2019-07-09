167ab02ceaa0872ee6e1b5219155d3b4a21e7f27
lipengzhou <lpzmail@163.com>
2019-07-09 08:58:44 +0800

    REM适配-配置flexible

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+/**
+ * 这个第三包引入即可，它会自动根据页面宽度设置网页基准字体大小
+ */
+import 'amfe-flexible'
+
 import {
   Button,
   Tabbar,
