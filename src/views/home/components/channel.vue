3068f791d24ee9d103b6d8afb65b222d468cc169
lipengzhou <lpzmail@163.com>
2019-07-12 12:11:44 +0800

    频道管理-处理编辑按钮的联动效果

--- src/views/home/components/channel.vue
+++ src/views/home/components/channel.vue
@@ -24,7 +24,8 @@
             type="danger"
             plain
             size="mini"
-          >编辑</van-button>
+            @click="isEdit = !isEdit"
+          >{{ isEdit ? '完成' : '编辑' }}</van-button>
         </div>
       </div>
       <van-grid class="channel-content" :gutter="10" clickable>
@@ -34,9 +35,9 @@
           text="文字">
           <span
             class="text"
-            :class="{ active: index === activeIndex }"
+            :class="{ active: index === activeIndex && !isEdit }"
           >{{ item.name }}</span>
-          <!-- <van-icon class="close-icon" name="close" /> -->
+          <van-icon class="close-icon" v-show="isEdit" name="close" />
         </van-grid-item>
       </van-grid>
     </div>
@@ -89,7 +90,8 @@ export default {
 
   data () {
     return {
-      allChannels: []
+      allChannels: [],
+      isEdit: false
     }
   },
 
